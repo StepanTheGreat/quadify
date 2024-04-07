b3a3c467cf50d454ff71a82b8c2b1ec9ec8f7886
StepanTheGreat <117771383+StepanTheGreat@users.noreply.github.com>
2024-04-07 14:17:49 +0200

    Really basic, manual tests (Will change in the future)

--- tests/main.rs
+++ tests/main.rs
@@ -1,1 +1,30 @@
-// Tests for main functionality
+/// This test tests for main functionality.
+/// The app should start, wait 4 seconds and then gracefully quit
+
+use quadify::prelude::*;
+use bevy::app::AppExit;
+
+#[derive(Resource)]
+struct ExitTimer(Timer);
+
+#[test]
+fn main() {
+    App::new()
+        .add_plugins(QuadifyPlugins)
+        .insert_resource(ExitTimer(
+            Timer::from_seconds(1.0, TimerMode::Once)
+        ))
+        .add_systems(Update, run_timer)
+    .run();
+}
+
+fn run_timer(
+    time: Res<Time>,
+    mut timer: ResMut<ExitTimer>,
+    mut exit_events: EventWriter<AppExit>
+) {
+    timer.0.tick(time.delta());
+    if timer.0.just_finished() {
+        exit_events.send(AppExit);
+    }
+}
