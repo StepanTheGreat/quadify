f1649835f2ad3fde55d762dd711becc7b8d89fb2
StepanTheGreat <117771383+StepanTheGreat@users.noreply.github.com>
2024-04-07 14:15:35 +0200

    Use PluginGroup instead for Quadify

--- src/lib.rs
+++ src/lib.rs
@@ -2,63 +2,35 @@
 // ! uses bevy's parallel processing plugin. Macroquad is designed to work on a single thread, thus
 // ! there needs to be some sort of isolation for ALL of its functionality.
 
-use bevy::prelude::*;
+use bevy::{a11y::AccessibilityPlugin, app::PluginGroupBuilder, diagnostic::DiagnosticsPlugin, input::InputPlugin, log::LogPlugin, prelude::*, time::TimePlugin};
 use macroquad::prelude::*;
 
-pub use macroquad; // Only import it if you actually need it
-use sprite::RenderingPlugin;
-use window::{WindowConfig, WindowPlugin, WindowSize};
+pub use macroquad;
+use window::MQWindowPlugin; // Only import it if you actually need it
+// use sprite::RenderingPlugin;
 
-pub mod sprite;
+// pub mod sprite;
 pub mod window;
 pub mod prelude;
 
-use resources::*;
+/// This collection of plugins is a custom made DefaultPlugins bundle
+pub struct QuadifyPlugins;
+impl PluginGroup for QuadifyPlugins {
+    fn build(self) -> PluginGroupBuilder {
+        PluginGroupBuilder::start::<Self>()
+            .add(LogPlugin::default())
+            .add(TaskPoolPlugin::default())
+            .add(TypeRegistrationPlugin)
+            .add(FrameCountPlugin)
+            .add(TimePlugin)
+            .add(TransformPlugin)
+            .add(HierarchyPlugin)
+            .add(DiagnosticsPlugin)
+            .add(InputPlugin)
+            .add(WindowPlugin::default())
+            .add(AccessibilityPlugin)
 
-pub struct QuadifyPlugin;
-impl Plugin for QuadifyPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_plugins(DefaultResourcesPlugin);
-        app.add_plugins(WindowPlugin);
-        app.add_plugins(RenderingPlugin);
-        app.set_runner(macro_runner);
+            // ? Custom Quadify Plugins. Planning to limit them by features
+            .add(MQWindowPlugin::default())
     }
-}
-
-fn macro_runner(mut app: App) {
-    let conf: Conf = if let Some(ref mut conf) = app.world.get_resource_mut::<WindowConfig>() {
-        conf.0.take().unwrap()
-    } else {
-        Conf {
-            window_title: "Quadify App".to_owned(),
-            window_width: WindowSize::default().width as i32,
-            window_height: WindowSize::default().height as i32,
-            ..Default::default()
-        }
-    };
-
-    macroquad::Window::from_config(conf, async move {
-        app.world.insert_resource::<WindowSize>(WindowSize { 
-            width: screen_width(), 
-            height: screen_height() 
-        });
-
-        app.finish();
-        app.cleanup();
-
-        loop {
-            // Should probably be moved somewhere else, except for AppExitRequested
-            if let Some(exit) = app.world.get_resource::<AppExitRequested>() {
-                if exit.0 {
-                    break
-                }
-            }
-            if let Some(ref mut dt) = app.world.get_resource_mut::<FrameTime>() {
-                dt.0 = get_frame_time();
-            }
-
-            app.update();
-            next_frame().await;
-        }
-    });
 }
