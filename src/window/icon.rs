b4a07669c684f88496313b1895d406dbfd6f2416
sokorototo <nyachiengatoto@gmail.com>
2024-05-07 12:03:57 +0300

    implemented WindowIcon

---
+++ src/window/icon.rs
@@ -0,0 +1,56 @@
+use image::DynamicImage;
+use miniquad::{conf::Icon, fs};
+use std::io;
+
+/// More ergonomic interface to [`Icon`].
+pub struct WindowIcon(image::DynamicImage);
+
+impl WindowIcon {
+	/// Load an icon from a file path
+	pub fn from_path<A: AsRef<str>, F: Fn(Result<WindowIcon, fs::Error>) + 'static>(path: A, callback: F) {
+		fs::load_file(path.as_ref(), move |res| match res {
+			// File was unable to load
+			Err(e) => callback(Err(e)),
+			Ok(data) => callback(WindowIcon::from_bytes(&data)),
+		});
+	}
+
+	/// Load an icon from a byte array
+	pub fn from_bytes(data: &[u8]) -> Result<WindowIcon, fs::Error> {
+		let data = io::Cursor::new(data);
+		let reader = image::io::Reader::new(data).with_guessed_format().map_err(fs::Error::IOError)?;
+		let img = reader.decode().map_err(|_| fs::Error::IOSAssetNoData)?;
+
+		Ok(WindowIcon(img))
+	}
+}
+
+fn downsample<const P: usize, const W: usize>(img: &DynamicImage) -> Option<[u8; P]> {
+	let height = P / W;
+	let thumbnail = img.thumbnail(W as _, height as _);
+
+	match thumbnail {
+		image::DynamicImage::ImageRgba8(rgba8) => {
+			let bytes = rgba8.into_raw();
+			let mut result: [u8; P] = [0; P];
+			result.copy_from_slice(&bytes);
+			Some(result)
+		}
+		_ => None,
+	}
+}
+
+impl TryFrom<WindowIcon> for Icon {
+	type Error = &'static str;
+
+	fn try_from(value: WindowIcon) -> Result<Self, Self::Error> {
+		static INVALID_IMAGE_FORMAT: &str = "Invalid image format, expected RGBA8 image";
+		let img = &value.0;
+
+		Ok(Icon {
+			small: downsample::<1024, 16>(img).ok_or(INVALID_IMAGE_FORMAT)?,
+			medium: downsample::<4096, 32>(img).ok_or(INVALID_IMAGE_FORMAT)?,
+			big: downsample::<16384, 64>(img).ok_or(INVALID_IMAGE_FORMAT)?,
+		})
+	}
+}
--- src/window/mod.rs
+++ src/window/mod.rs
@@ -1,18 +1,16 @@
 use bevy_app::{App, Plugin};
 use miniquad::conf::Conf;
-use miniquad::RenderingBackend;
+use miniquad::RenderingBackend as MqdRenderingBackend;
 use miniquad::{window, EventHandler};
 
-/// Miniquad rendering backend object. Initialize ONLY after [`miniquad::start`]
-pub struct Graphics {
-	pub backend: Box<dyn RenderingBackend>,
-}
+mod icon;
 
-impl Graphics {
+/// Miniquad rendering backend object. Initialize ONLY after [`miniquad::start`]
+pub struct RenderingBackend(Box<dyn MqdRenderingBackend>);
+
+impl RenderingBackend {
 	pub fn new() -> Self {
-		Self {
-			backend: window::new_rendering_backend(),
-		}
+		Self(window::new_rendering_backend())
 	}
 }
 
@@ -23,7 +21,6 @@ struct QuadState {
 
 impl EventHandler for QuadState {
 	fn update(&mut self) {}
-
 	fn draw(&mut self) {
 		// ! Updating the entire app here for now, but in the future there should be 2 different schedules for both `update` and `draw`
 		self.app.update();
@@ -37,6 +34,7 @@ pub struct WindowPlugin {
 	pub height: i32,
 	pub fullscreen: bool,
 	pub high_dpi: bool,
+	// pub icon: Option<Icon>
 }
 
 impl WindowPlugin {
@@ -74,7 +72,7 @@ impl Plugin for WindowPlugin {
 
 fn miniquad_runner(mut app: App, conf: Conf) {
 	miniquad::start(conf, move || {
-		app.insert_non_send_resource(Graphics::new());
+		app.insert_non_send_resource(RenderingBackend::new());
 		Box::new(QuadState { app })
 	});
 }
