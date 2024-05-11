ff57fb1188657ebc6de25fbaa481a96db71d0ade
sokorototo <nyachiengatoto@gmail.com>
2024-05-11 20:56:12 +0300

    use `oneshot` for asset io

--- src/io.rs
+++ src/io.rs
@@ -1,22 +1,25 @@
+use std::{io, task};
+
 use miniquad::fs;
-use std::sync::mpsc::{sync_channel, Receiver};
+use oneshot::Receiver;
 
 struct FileLoadingFuture(Receiver<Result<Vec<u8>, fs::Error>>);
 
 impl std::future::Future for FileLoadingFuture {
 	type Output = Result<Vec<u8>, fs::Error>;
 
-	fn poll(self: std::pin::Pin<&mut Self>, _: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
+	fn poll(self: std::pin::Pin<&mut Self>, _: &mut task::Context<'_>) -> task::Poll<Self::Output> {
 		match self.0.try_recv() {
-			Ok(res) => std::task::Poll::Ready(res),
-			Err(_) => std::task::Poll::Pending,
+			Ok(res) => task::Poll::Ready(res),
+			Err(oneshot::TryRecvError::Empty) => task::Poll::Pending,
+			Err(oneshot::TryRecvError::Disconnected) => task::Poll::Ready(Err(fs::Error::IOError(io::Error::new(io::ErrorKind::Other, "File loading future was dropped")))),
 		}
 	}
 }
 
 /// Load a file from the filesystem or http on the web
 pub async fn load_file(path: &str) -> Result<Vec<u8>, fs::Error> {
-	let (sender, receiver) = sync_channel(1);
+	let (sender, receiver) = oneshot::channel();
 	fs::load_file(path, move |res| {
 		let res = res.map(|mut data| {
 			data.shrink_to_fit();
