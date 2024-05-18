e4acc17857765d5e5727ccc36507b241083a4dc2
sokorototo <nyachiengatoto@gmail.com>
2024-05-18 15:55:22 +0300

    added `new` to Rgba

--- src/render/rgba.rs
+++ src/render/rgba.rs
@@ -7,6 +7,12 @@ pub struct Rgba {
 	pub a: f32,
 }
 
+impl Rgba {
+	pub fn new(r: f32, g: f32, b: f32, a: f32) -> Self {
+		Self { r, g, b, a }
+	}
+}
+
 pub fn rgba(r: f32, g: f32, b: f32, a: f32) -> Rgba {
 	Rgba { r, g, b, a }
 }
