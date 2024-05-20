7ef9f5fd4ddaa30b10aae7ec8de7179b589a2208
StepanTheGreat <117771383+StepanTheGreat@users.noreply.github.com>
2024-05-20 15:06:06 +0200

    Way more simplified mesh example, that finally looks nice and utilizes macroquad's batch rendering

--- tests/mesh.rs
+++ tests/mesh.rs
@@ -2,16 +2,17 @@
 /// Thus this test is only for mesh demonstration purposes.
 
 use bevy::prelude::*;
-use miniquad::*;
-use quadify::prelude::geometry::Vertex;
+use bevy_input::mouse::MouseButtonInput;
+use quadify::color::rgba;
+use quadify::prelude::geometry::{Mesh, Vertex};
 use quadify::{color, prelude::*};
 use quadify::prelude::WindowPlugin;
 use quadify::prelude::RenderingBackend;
 
-struct RenderState {
-    pipeline: Pipeline,
-    bindings: Bindings,
-    verts: i32
+#[derive(Resource)]
+struct MeshHandle {
+    mesh: Mesh,
+    parts_count: u32
 }
 
 #[test]
@@ -26,102 +27,38 @@ fn main() {
 			..Default::default()
 		}))
         .add_systems(Startup, setup_render_state)
+        .add_systems(Update, change_on_click)
 		.add_systems(MiniquadDraw, draw_circle)
 		.run();
 }
 
-fn setup_render_state(world: &mut World) {
-    let mut backend = world.get_non_send_resource_mut::<RenderingBackend>().unwrap();
-    let mesh = geometry::Mesh::circle(64, color::RED);
-
-    let vbo = backend.new_buffer(
-        BufferType::VertexBuffer, 
-        BufferUsage::Immutable, 
-        BufferSource::slice(&mesh.vertices[..])
-    );
-    
-    let ibo = backend.new_buffer(
-        BufferType::IndexBuffer, 
-        BufferUsage::Immutable, 
-        BufferSource::slice(&mesh.indices[..])
-    );
-
-    let shader = backend.new_shader(
-        ShaderSource::Glsl { 
-            vertex: VERTEX_SHADER, 
-            fragment: FRAGMENT_SHADER 
-        },
-        ShaderMeta { 
-            uniforms: UniformBlockLayout { uniforms: vec![] }, 
-            images: vec![]
-        }
-    ).unwrap();
-
-    let pipeline = backend.new_pipeline(
-        &[
-            BufferLayout::default()
-        ], 
-        &Vertex::attributes(),
-        shader, 
-        PipelineParams::default()
-    );
-
-    let bindings = Bindings {
-        vertex_buffers: vec![vbo],
-        index_buffer: ibo,
-        images: vec![]
-    };
-
-    world.insert_non_send_resource(RenderState {
-        pipeline,
-        bindings,
-        verts: mesh.indices.len() as i32
+fn setup_render_state(mut commands: Commands) {
+    commands.insert_resource(MeshHandle {
+        mesh: geometry::Mesh::circle(4, color::RED),
+        parts_count: 4
     });
 }
 
+fn change_on_click(
+    mut mesh: ResMut<MeshHandle>,
+    mut click: EventReader<MouseButtonInput>
+) {
+    for event in click.read() {
+        if event.state.is_pressed() {
+            mesh.parts_count = ((mesh.parts_count + 1) % 16).max(4);
+            mesh.mesh = geometry::Mesh::circle(mesh.parts_count, color::RED);
+        }
+    }
+}
+
 fn draw_circle(
-    render_state: NonSend<RenderState>,
+    mesh: Res<MeshHandle>,
     mut render_ctx: NonSendMut<RenderingBackend>
 ) {
-    let (pipeline, bindings, verts) = {
-        (render_state.pipeline.clone(), render_state.bindings.clone(), render_state.verts)
-    };
-
-    render_ctx.begin_default_pass(PassAction::Nothing);
-    render_ctx.apply_pipeline(&pipeline);
-    render_ctx.apply_bindings(&bindings);
-    render_ctx.draw(0, verts, 1);
-    render_ctx.end_render_pass();
-    
-    // backend.begin_default_pass(PassAction::Clear { 
-    //     color: Some((0.0, 0.0, 0.0, 1.0)), 
-    //     depth: None, 
-    //     stencil: None 
-    // });
-    
+    let (verts, inds) = (&mesh.mesh.vertices, &mesh.mesh.indices);
+    render_ctx.clear(rgba(0, 0, 0, 0));
+    render_ctx.texture(None);
+    render_ctx.draw_mode(pipeline::DrawMode::Triangles);
+    render_ctx.geometry(&verts[..], &inds[..]);
+    render_ctx.draw(Mat4::IDENTITY);
 }
-
-const VERTEX_SHADER: &str = r#"#version 100
-    precision lowp float;
-
-    attribute vec3 in_pos;
-    attribute vec2 in_uv;
-    attribute vec4 in_color;
-
-    varying lowp vec4 color;
-
-    void main() {
-        color = in_color;
-        gl_Position = vec4(in_pos, 1.0);
-    }
-"#;
-
-const FRAGMENT_SHADER: &str = r#"#version 100
-precision lowp float;
-
-varying lowp vec4 color;
-
-void main() {
-    gl_FragColor = color;
-}
-"#;
