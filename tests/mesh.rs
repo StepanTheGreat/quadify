c9cd428766efa31504fef3edc2363c2a6726a094
StepanTheGreat <117771383+StepanTheGreat@users.noreply.github.com>
2024-06-03 22:02:03 +0200

    Fixed mesh example with asset handles now

--- tests/mesh.rs
+++ tests/mesh.rs
@@ -1,10 +1,10 @@
 /// This test should be changed in the future to use more higher-level functionality like `Material`s, to avoid writing repetitive shaders;
 /// Thus this test is only for mesh demonstration purposes.
 use bevy_app::prelude::*;
+use bevy_asset::{Assets, Handle};
 use bevy_ecs::prelude::*;
 use bevy_input::mouse::MouseButtonInput;
-use quadify::color::{rgba, WHITE};
-use quadify::prelude::geometry::{Mesh, MeshBuilder, Vertex};
+use quadify::prelude::geometry::{Mesh, MeshBuilder};
 use quadify::prelude::RenderingBackend;
 use quadify::prelude::WindowPlugin;
 use quadify::{color, prelude::*};
@@ -12,7 +12,7 @@ use glam::Mat4;
 
 #[derive(Resource)]
 struct MeshHandle {
-	mesh: Mesh,
+	mesh: Handle<Mesh>,
 	parts_count: u32,
 }
 
@@ -27,43 +27,53 @@ fn main() {
 			resizeable: false,
 			..Default::default()
 		}))
-
 		.add_systems(Startup, setup_render_state)
 		.add_systems(Update, change_on_click)
 		.add_systems(MiniquadDraw, draw_circle)
 		.run();
 }
 
-fn setup_render_state(mut commands: Commands) {
-	let mesh = MeshBuilder::new()
-		.as_circle(0.2)
-		.circle_points(3)
-		.with_color(WHITE)
-		.build();
+fn setup_render_state(
+	mut commands: Commands,
+	mut meshes: ResMut<Assets<Mesh>>
+) {
 	commands.insert_resource(MeshHandle {
-		mesh,
+		mesh: meshes.add(MeshBuilder::new()
+			.as_circle(0.2)
+			.circle_points(3)
+			.build()),
 		parts_count: 3,
 	});
 }
 
-fn change_on_click(mut mesh: ResMut<MeshHandle>, mut click: EventReader<MouseButtonInput>) {
+fn change_on_click(
+	mut mesh: ResMut<MeshHandle>, 
+	mut click: EventReader<MouseButtonInput>,
+	mut meshes: ResMut<Assets<Mesh>>
+) {
 	for event in click.read() {
 		if event.state.is_pressed() {
 			mesh.parts_count = ((mesh.parts_count + 1) % 64).max(4);
-			mesh.mesh = MeshBuilder::new()
+			meshes.insert(&mesh.mesh, MeshBuilder::new()
 				.as_circle(0.2)
 				.circle_points(mesh.parts_count)
-				.with_color(WHITE)
-				.build();
+				.build());
 		}
 	}
 }
 
-fn draw_circle(mesh: Res<MeshHandle>, mut render_ctx: NonSendMut<RenderingBackend>) {
-	let (verts, inds) = (&mesh.mesh.vertices, &mesh.mesh.indices);
-	render_ctx.clear(color::rgba(0, 0, 0, 0));
-	render_ctx.texture(None);
-	render_ctx.draw_mode(pipeline::DrawMode::Triangles);
-	render_ctx.geometry(&verts[..], &inds[..]);
-	render_ctx.draw(Mat4::IDENTITY);
+fn draw_circle(
+	mesh: Res<MeshHandle>, 
+	mut render_ctx: NonSendMut<RenderingBackend>,
+	meshes: Res<Assets<Mesh>>
+) {
+
+	if let Some(mesh) = meshes.get(&mesh.mesh) {
+		let (verts, inds) = (&mesh.vertices, &mesh.indices);
+		render_ctx.clear(color::rgba(0, 0, 0, 0));
+		render_ctx.texture(None);
+		render_ctx.draw_mode(pipeline::DrawMode::Triangles);
+		render_ctx.geometry(&verts[..], &inds[..]);
+		render_ctx.draw(Mat4::IDENTITY);
+	}
 }
